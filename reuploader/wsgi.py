53b3144a4a4ff3b80b2daa37e2fb83ef049f6374
Viliam <v.popovec@gmail.com>
2023-05-03 23:05:44 +0200

    init_db in wsgi

--- reuploader/wsgi.py
+++ reuploader/wsgi.py
@@ -1,3 +1,11 @@
 from reuploader import create_app
+from reuploader.db import init_db
+from sqlite3 import OperationalError
 
 app = create_app()
+
+try:
+    with app.app_context():
+        init_db()
+except OperationalError:
+    pass
